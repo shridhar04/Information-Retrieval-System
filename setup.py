1058866c552f240ac699d9724cae11d1e332874e
Shridhar <shridharpatil461997@gmail.com>
2026-01-19 15:19:24 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages , setup
+
+setup(
+    name =  'Generative AI AProjects',
+    version =  '0.0.0',
+    author = 'Shridhar',
+    author_email = 'Shridhar@ineuron.ai',
+    packages =  find_packages(),
+    install_requires = []
+)
